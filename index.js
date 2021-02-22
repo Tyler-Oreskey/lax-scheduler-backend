405b3581935e794081509d8f70c927fcaa9fa616
tyler-oreskey <toreskey@gmail.com>
2021-02-22 15:15:42 -0700

    added reusable validation and error handling. added validation and error handling for games routes.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-
-const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const config = require("./src/config");
 const controllers = require("./src/controllers");
+const { handleError } = require("./src/middleware/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(cors({ origin: config.origin }));
 app.get("/health", (req, res) => res.send("OK!"));
 app.use(controllers(router));
+app.use((err, req, res, next) => handleError(err, res));
 
 app.listen(config.port, () => console.log(`App listeing on port ${config.port}`))
--- src/routes/games/index.js
+++ src/routes/games/index.js
@@ -1,43 +1,91 @@
 const queries = require("../../database/games");
 
+const { ErrorHandler } = require("../../middleware/error");
+const validation = require("../../validation");
+
 module.exports = {
     getAll: async (req, res, next) => {
         try {
             const allGames = await queries.getAll();
             return res.status(200).json(allGames);
         } catch (error) {
+            next(error);
         }
     },
     getByID: async (req, res, next) => {
         try {
             const { id } = req.params;
+            const isValidID = validation.validateNumber(id);
+
+            if (!isValidID) {
+                throw new ErrorHandler(400, "Invalid ID provided!");
+            }
+
             const singleGame = await queries.getByID(id);
             return res.status(200).json(singleGame);
         } catch (error) {
+            next(error);
         }
     },
     create: async (req, res, next) => {
         try {
             const { body } = req;
+            const isValidBody = validation.validateObjectTypes(body, {
+                name: String,
+                location: String,
+                start_date: String,
+                end_date: String
+            }, lengthStrict = true);
+
+            if (!isValidBody) {
+                throw new ErrorHandler(400, "Invalid request body!");
+            }
+
             await queries.create(body);
             return res.status(200).json({ message: "Success!" });
         } catch (error) {
+            next(error);
         }
     },
     updateByID: async (req, res, next) => {
         try {
             const { body, params: { id } } = req;
+            const isValidID = validation.validateNumber(id);
+
+            if (!isValidID) {
+                throw new ErrorHandler(400, "Invalid ID provided!");
+            }
+
+            const isValidBody = validation.validateObjectTypes(body, {
+                name: String,
+                location: String,
+                start_date: String,
+                end_date: String
+            });
+
+            if (!isValidBody) {
+                throw new ErrorHandler(400, "Invalid request body!");
+            }
+
             await queries.updateByID(id, body)
             return res.status(200).json({ message: "Success!" });
         } catch (error) {
+            next(error);
         }
     },
     deleteByID: async (req, res, next) => {
         try {
             const { id } = req.params;
+            const isValidID = validation.validateNumber(id);
+
+            if (!isValidID) {
+                throw new ErrorHandler(400, "Invalid ID provided!");
+            }
+
             await queries.deleteByID(id);
             return res.status(200).json({ message: "Success!" });
         } catch (error) {
+            next(error);
         }
     }
 };
