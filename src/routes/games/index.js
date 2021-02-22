3708369aa515d9758da5c2e2f87bac550e88b632
tyler-oreskey <toreskey@gmail.com>
2021-02-22 14:19:54 -0700

    added routes for games. added env and node modules in gitignore

--- src/routes/games/index.js
+++ src/routes/games/index.js
@@ -1,5 +1,43 @@
+const queries = require("../../database/games");
+
 module.exports = {
     getAll: async (req, res, next) => {
-        return res.status(200).json({ message: "Returned all games!" });
+        try {
+            const allGames = await queries.getAll();
+            return res.status(200).json(allGames);
+        } catch (error) {
+        }
+    },
+    getByID: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const singleGame = await queries.getByID(id);
+            return res.status(200).json(singleGame);
+        } catch (error) {
+        }
+    },
+    create: async (req, res, next) => {
+        try {
+            const { body } = req;
+            await queries.create(body);
+            return res.status(200).json({ message: "Success!" });
+        } catch (error) {
+        }
+    },
+    updateByID: async (req, res, next) => {
+        try {
+            const { body, params: { id } } = req;
+            await queries.updateByID(id, body)
+            return res.status(200).json({ message: "Success!" });
+        } catch (error) {
+        }
+    },
+    deleteByID: async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            await queries.deleteByID(id);
+            return res.status(200).json({ message: "Success!" });
+        } catch (error) {
+        }
     }
-}
+};
