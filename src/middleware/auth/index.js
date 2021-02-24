b780bd19154c38445d815db866c6f8a9c5731bbc
tyler-oreskey <toreskey@gmail.com>
2021-02-23 17:29:10 -0700

    added user login route. Added bcrypt password compare function.

--- src/middleware/auth/index.js
+++ src/middleware/auth/index.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 
 module.exports = {
-    generateHashedPassword: (password) => bcrypt.hash(password, 10)
+    generateHashedPassword: (password) => bcrypt.hash(password, 10),
+    compareHashedPassword: (password, dbPassword) => bcrypt.compare(password, dbPassword)
 };
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,7 +2,7 @@ const CommonRoutes = require("../common");
 const { ErrorHandler } = require("../../middleware/error");
 const { userGames, user } = require("../../database");
 const validation = require("../../validation");
-const { generateHashedPassword } = require("../../middleware/auth");
+const { generateHashedPassword, compareHashedPassword } = require("../../middleware/auth");
 
 class User extends CommonRoutes {
     constructor(...args) {
@@ -41,6 +41,28 @@ class User extends CommonRoutes {
             next(error);
         }
     };
+
+    login = async (req, res, next) => {
+        try {
+            const isValidEmail = validation.validateEmail(req.body.email);
+
+            if (!isValidEmail) {
+                throw new ErrorHandler(400, "Invalid email!");
+            }
+
+            const foundUser = await user.getByEmailNoPass(req.body.email);
+            const foundPassword = await user.getPasswordByEmail(req.body.email);
+            const isCorrectPassword = await compareHashedPassword(req.body.password, foundPassword.password);
+
+            if (!isCorrectPassword) {
+                throw new ErrorHandler(400, "Invalid user!");
+            }
+
+            return res.status(200).json(foundUser);
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 module.exports = User;
