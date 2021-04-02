dfd1f956840dfda5c524a43c935f7a28f3b7406d
tyler-oreskey <toreskey@gmail.com>
2021-04-02 12:49:27 -0600

    added route middleware to authenticate JWT tokens

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const config = require('./src/config');
 const { handleError } = require('./src/middleware/error');
 const bootstrap = require('./src/bootstrap');
+const { JWT } = require('./src/middleware/auth');
 
 const app = express();
 const router = express.Router();
 
+// use jwt validation middleware
+app.use((req, res, next) => new JWT().authenticated(req, res, next));
+
+// cors configuration
 const cors = require('cors');
 app.use(cors({ origin: config.origin }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// bootstraps root of router
 bootstrap(app, router);
 
+// error handler
 app.use((err, req, res, next) => handleError(err, res));
 
+// for aws health checks
 app.get('/health', (req, res) => res.send('OK!'));
 
 app.listen(config.port);
